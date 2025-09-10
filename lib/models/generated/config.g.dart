e2b69d3a740ebf9ee7291b8775810af638f4c2e1
kysornekmc <53962494+kysornekmc@users.noreply.github.com>
2025-09-10 21:08:05 +0800

    153、仪表盘顶部运行时长开关显示

--- lib/models/generated/config.g.dart
+++ lib/models/generated/config.g.dart
@@ -69,7 +69,7 @@ const _$RecoveryStrategyEnumMap = {
   RecoveryStrategy.override: 'override',
 };
 
-const _$DashboardWidgetEnumMap = {
+const _$DashboardWidgetEnumMap = {  //这里序列化后实际上就是待选卡片的顺序
   DashboardWidget.networkSpeed: 'networkSpeed',
   DashboardWidget.networkSpeedMultiLine: 'networkSpeedMultiLine',
   DashboardWidget.subscriptionInformationdetail:
